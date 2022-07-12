eb26428152a5527e4f7808918a0b3e296cae3e3d
danishaft <danielejeh2019@gmail.com>
2022-07-12 21:32:54 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -31,3 +31,61 @@ function passwordToggleTwo(){
     eyeClosed.style.display = "block";  
   }
 }
+
+$(document).ready(function(){
+  $("body").on("click",".table-guest-name",function(){
+    $("#adminTableModal").addClass("position-absolute");
+    $(".modal-backdrop").addClass("position-absolute");
+    $("#adminTableModal").modal("show");
+    $('.modal-backdrop').appendTo('.main-content');
+    $('body').removeClass("modal-open")
+    $('body').css("padding-right","");     
+  });
+});
+
+let tabBtns = document.querySelectorAll('.tab-btn');
+let tabPages = document.querySelectorAll('.tab-page');
+
+tabBtns.forEach(tabBtn => {
+  tabBtn.addEventListener("click",function() {
+    for (let i = 0; i < tabBtns.length; i++) {
+      tabBtns[i].classList.remove('current');
+      tabPages[i].classList.add('hide');
+    }
+
+    let pageTarget = document.querySelector(`${tabBtn.dataset.page}`);
+
+    tabBtn.classList.add('current');
+    pageTarget.classList.remove('hide');
+  })
+})
+
+
+let dropdownBtn1 = document.querySelector('#sort-btn-1');
+let dropdown1 = document.querySelector('#sort-dropdown-1');
+
+dropdownBtn1.addEventListener("click", function () {
+  dropdown1.classList.toggle("hide");
+});
+
+let dropdowns = document.querySelectorAll('.btn-dropdown');
+let dropdownItems = document.querySelectorAll('.dropdown-item');
+
+dropdownItems.forEach(dropdownItem => {
+  dropdownItem.addEventListener("click", function() {
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.add("hide");
+    }
+  });
+});
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-button') && !event.target.matches('.dropdown-button-text') && !event.target.matches('.dropdown-button-span') && !event.target.matches('.dropdown-button-icon')) {
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (!openDropdown.classList.contains('hide')) {
+        openDropdown.classList.add('hide');
+      }
+    }
+  }
+}
